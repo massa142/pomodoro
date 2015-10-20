38037d631eaab315fe8f270473d69bc3b4f1a883
massa142 <mstk214@gmail.com>
2015-10-21 08:29:31 +0900

    Copy Quramy/electron-jsx-babel-boilerplate

---
+++ src/renderer/components/main.jsx
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+import shell from 'shell';
+
+export class Main extends React.Component {
+  state = {
+    message: 'Hello, Electron'
+  }
+  //constructor () {
+  //  super();
+  //  this.openGithub = ::this.openGithub
+  //}
+  openGithub () {
+    shell.openExternal('https://github.com/Quramy/electron-jsx-babel-boilerplate');
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron main">
+          <h1>{this.state.message}</h1>
+          <img src="../assets/images/electron.svg" alt="" width="128px"></img>
+          <p>Provided by <a href="#" onClick={this.openGithub}>electron-jsx-babel-boilerplate<span className="glyphicon glyphicon-heart"></span></a></p>
+        </div>
+      </div>
+    );
+  }
+}
