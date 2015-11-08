1f3204c65531dea454b90bb00fa00b6c7d333d9d
massa142 <mstk214@gmail.com>
2015-11-09 05:35:37 +0900

    v1.0

--- src/renderer/components/main.jsx
+++ src/renderer/components/main.jsx
@@ -1,28 +1,105 @@
 'use strict';
 
 import React from 'react';
-import shell from 'shell';
+import notifier from 'node-notifier';
 
 export class Main extends React.Component {
-  state = {
-    message: 'Hello, Electron'
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStart: false,
+      isBreak: false,
+      time: this.props.duration
+    }
+    this.handleStartClicked = ::this.handleStartClicked;
+    this.handleResetClicked = ::this.handleResetClicked;
+    this.tick = ::this.tick;
   }
-  //constructor () {
-  //  super();
-  //  this.openGithub = ::this.openGithub
-  //}
-  openGithub () {
-    shell.openExternal('https://github.com/Quramy/electron-jsx-babel-boilerplate');
+
+  handleStartClicked() {
+    if (!this.state.isStart) {
+      this.interval = setInterval(this.tick, 1000);
+      this.setState({isStart: true});
+    } else {
+      clearInterval(this.interval);
+      this.setState({isStart: false});
+    }
   }
+
+  handleResetClicked() {
+    this.reset();
+  }
+
+  tick() {
+    this.setState({time: this.state.time - 1});
+    if (this.state.time === 0) {
+      this.finishEvent();
+    }
+  }
+
+  finishEvent() {
+    if (this.state.isBreak) {
+      this.reset();
+      this.notify('Break is over!');
+    } else {
+      this.break();
+      this.notify('Good work!');
+    }
+  }
+
+  break() {
+    this.setState({
+      isStart: true,
+      isBreak: true,
+      time: this.props.breakTime
+    });
+  }
+
+  reset() {
+    clearInterval(this.interval);
+    this.setState({
+      isStart: false,
+      isBreak: false,
+      time: this.props.duration
+    });
+  }
+
+  notify(message) {
+    notifier.notify({
+      'title': 'Pomodoro Timer',
+      'message': message
+    });
+  }
+
+  converter = {
+    s2m(s) {
+      let minutes = Math.floor(s / 60);
+      let seconds = s % 60;
+      return ('0' + minutes.toString()).slice(-2) + ':' + ('0' + seconds.toString()).slice(-2);
+    }
+  };
+
   render() {
     return (
       <div className="container">
         <div className="jumbotron main">
-          <h1>{this.state.message}</h1>
-          <img src="../assets/images/electron.svg" alt="" width="128px"></img>
-          <p>Provided by <a href="#" onClick={this.openGithub}>electron-jsx-babel-boilerplate<span className="glyphicon glyphicon-heart"></span></a></p>
+          <h2>Pomodoro Timer</h2>
+          <img src={this.state.isBreak ? "../assets/images/electron2.svg" : "../assets/images/electron.svg"} alt="" width="128px"></img>
+          <h2>{this.converter.s2m(this.state.time)}</h2>
+          <div>
+            <button type="button" className="btn btn-primary" onClick={this.handleStartClicked}>
+              {this.state.isStart ? "Pause" : "Start"}
+            </button>
+            <button type="button" className="btn btn-warning" onClick={this.handleResetClicked}>Reset</button>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+Main.defaultProps = {
+  duration: 1500,
+  breakTime: 300
+};
